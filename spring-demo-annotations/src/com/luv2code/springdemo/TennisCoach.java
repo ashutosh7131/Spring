b0b7b304398e4deb021e8688ceffda0f21287cfc
Ashutosh Shriansh <ashutosh.shriansh@ibm.com>
2022-05-31 17:26:30 +0530

    annotation-config spring project commit

--- spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
@@ -1,22 +1,24 @@
 package com.luv2code.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component("thatSillyCoach")
 public class TennisCoach implements Coach {
 
+	@Autowired
+	@Qualifier("happyFortuneService")
 	private FortuneService fortuneService;
 
 	public TennisCoach() {
 		super();
 	}
 
-	@Autowired
-	public TennisCoach(FortuneService theFortuneService) {
-		super();
-		this.fortuneService = theFortuneService;
-	}
+	/*
+	 * @Autowired public TennisCoach(FortuneService theFortuneService) { super();
+	 * this.fortuneService = theFortuneService; }
+	 */
 
 	@Override
 	public String getDailyWorkout() {
