69ffa4b7dcf16b4d5c7d606ebc28ca4934bfbf1c
Ashutosh Shriansh <ashutosh.shriansh@ibm.com>
2022-05-31 13:26:32 +0530

    third commit

--- spring-demo-one/src/com/luv2code/springdemo/MyApp.java
+++ spring-demo-one/src/com/luv2code/springdemo/MyApp.java
@@ -5,7 +5,9 @@ public class MyApp {
 	public static void main(String[] args) {
 		
 		Coach theCoach = new BaseballCoach();
+		Coach theCoach1 = new TrackCoach();
 		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(theCoach1.getDailyWorkout());
 
 	}
 
